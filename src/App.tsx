93c3ec40d50d7bf223b203945cdbfb1734b91bd1
Praneeth Uppari <praneeth.uppari@hotmail.com>
2025-11-30 19:01:07 -0500

    change UI to include a darker theme with travel thematic elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import "./App.css";
 import { Header } from "./shared/components/Header";
 
 export function App() {
+  const location = useLocation();
+  const isLandingPage = location.pathname === "/";
+
   return (
     <main className="flex min-h-screen w-full flex-col bg-neutral-50 text-neutral-800">
-      <Header />
+      {!isLandingPage && <Header />}
       <Outlet />
     </main>
   );
